1b861fc1f24f6a0471ceb0d2573ab5e98970452a
Billy <billy.jakmall@gmail.com>
2020-12-23 11:51:06 +0700

    fix: implement slick on ulasan produk section

--- js/core.js
+++ js/core.js
@@ -1,5 +1,34 @@
 // @codekit-prepend "vendor/jquery-3.4.1.min.js";
 // @codekit-prepend "vendor/slick.min.js";
 
-document.querySelector("meta[name=viewport]").setAttribute('content', 'width=device-width, initial-scale='+(1/window.devicePixelRatio)+', maximum-scale=1.0, user-scalable=0');
-alert(window.devicePixelRatio);
+$(function(){
+  const hrlsSlider = $('.hrls__slides');
+  hrlsSlider.slick({
+    slidesToShow: 3,
+    dots: true,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
+  hrlsSlider.on('wheel', (function(e) {
+    e.preventDefault();
+    if (e.originalEvent.deltaY < 0) {
+      $(this).slick('slickNext');
+    } else {
+      $(this).slick('slickPrev');
+    }
+  }));
+})
