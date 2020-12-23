133aee10e2c68f615628209da00ffd8d913b4f78
Billy <billy.jakmall@gmail.com>
2020-12-23 11:13:14 +0700

    fix: add slick js plugin

--- js/core.js
+++ js/core.js
@@ -1,4 +1,5 @@
 // @codekit-prepend "vendor/jquery-3.4.1.min.js";
+// @codekit-prepend "vendor/slick.min.js";
 
 document.querySelector("meta[name=viewport]").setAttribute('content', 'width=device-width, initial-scale='+(1/window.devicePixelRatio)+', maximum-scale=1.0, user-scalable=0');
 alert(window.devicePixelRatio);
