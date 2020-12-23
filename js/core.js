2675fedebaf978d525077560ca0585bfe7947af6
Billy <billy.jakmall@gmail.com>
2020-12-23 14:44:56 +0700

    fix: add number animation when scrolling to the himo bike section

--- js/core.js
+++ js/core.js
@@ -33,3 +33,28 @@ $(function(){
     }
   }));
 })
+
+// home spec number animation
+$(window).on('scroll', function () {
+	var top_of_element = $(".hbo").offset().top;
+	var bottom_of_element = $(".hbo").offset().top + $(".hbo").outerHeight();
+	var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
+	var top_of_screen = $(window).scrollTop();
+
+	if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)) {
+		$('.hbo__feature__title__number').each(function (index, element) {
+      let initialPoint = index === 0 ? -20 : 0;
+			$(element).prop('Counter', initialPoint).animate({
+				Counter: $(element).text()
+			}, {
+				duration: 2500,
+				easing: 'swing',
+				step: function (now) {
+					$(element).text(Math.ceil(now));
+				}
+			});
+		});
+
+		$(window).unbind('scroll');
+	}
+})
