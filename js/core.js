e853097b4cec21359d4eb130ec737e1bc50b990c
Billy <billy.jakmall@gmail.com>
2020-12-28 17:14:06 +0700

    fix: add arrow for slider

--- js/core.js
+++ js/core.js
@@ -3,26 +3,21 @@
 
 $(function(){
   const sliderContainer = $('.hrls__slides, .hp__slides');
+  const productReviewSlides = $('.hrls__slides');
+  const productReviewNextArrow = $('.hrls__arrow--next');
+  const productReviewPrevArrow = $('.hrls__arrow--prev');
 
-  sliderContainer.slick({
-    slidesToShow: 3,
-    dots: true,
-    arrows: false,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2
-        }
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 1
-        }
-      }
-    ]
-  });
+  const recommendSlides = $('.hp__slides');
+  const recommendSlidesNextArrow = $('.hp__arrow--next');
+  const recommendSlidesPrevArrow = $('.hp__arrow--prev');
+
+  productReviewSlides.slick(
+    slickProps(productReviewPrevArrow ,productReviewNextArrow)
+  );
+
+  recommendSlides.slick(
+    slickProps(recommendSlidesNextArrow ,recommendSlidesPrevArrow)
+  );
 
   sliderContainer.on('wheel', (function(e) {
     e.preventDefault();
@@ -42,12 +37,35 @@ $(function(){
   })
 })
 
+function slickProps(prevSelector, nextSelector){
+  return {
+    slidesToShow: 3,
+    dots: true,
+    prevArrow: prevSelector,
+		nextArrow: nextSelector,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  }
+}
+
 // home spec number animation
 $(window).on('scroll', function () {
 	var top_of_element = $(".hbo").offset().top;
 	var bottom_of_element = $(".hbo").offset().top + $(".hbo").outerHeight();
-	var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
-	var top_of_screen = $(window).scrollTop();
+  var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
+  var top_of_screen = $(window).scrollTop();
 
 	if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)) {
 		$('.hbo__feature__title__number').each(function (index, element) {
